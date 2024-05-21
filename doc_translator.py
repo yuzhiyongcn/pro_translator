629d1afc4ffee47733ac68224eaab1102fd7d883
biggerfish <yuzhiyongcn@qq.com>
2024-05-21 17:33:17 +0800

    feat:  多线程翻译

--- doc_translator.py
+++ doc_translator.py
@@ -1,164 +1,139 @@
-from openai import OpenAI
 import os
 from docx import Document
 from gpt_translator import GPTTranslator
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, wait
 import json
+import shutil
+import time
 
 
-# 设置 OpenAI API 密钥
 class DocTranslator:
-    def __init__(self, num_threads=12):
+    def __init__(self, num_threads=16):
         self.num_threads = num_threads
-        terminology = {}
-        file = "config.json"
-        with open(file, "r", encoding="utf-8") as f:
-            terminology = json.load(f)
+        terminology = self.__load_terminology("terminology.txt")
+        print(f"术语表: {json.dumps(terminology, indent=4)}")
 
         self.translator = GPTTranslator(terminology)
 
+    def __load_terminology(self, file_path):
+        data = {}
+        with open(file_path, "r", encoding="utf-8") as file:
+            for line in file:
+                # 去除每行的空白符
+                line = line.strip()
+                if line:
+                    # 分割成键值对
+                    key, value = line.split("=", 1)
+                    # 去除键和值两端的空白符
+                    key = key.strip()
+                    value = value.strip()
+                    # 添加到字典中
+                    data[key] = value
+        return data
+
     def __translate_text(self, text):
-        # return self.translator.translate(text)
-        return text
+        return self.translator.translate(text)
+        # return text
 
-    def __translate_element(self, element, new_doc):
-        """
-        复制段落或表格到新文档中，并保持格式
-        """
-        if element.tag.endswith("p"):
-            new_paragraph = new_doc.add_paragraph()
-            new_paragraph.alignment = element.alignment
-            if hasattr(element, "paragraph_format"):
-                new_paragraph.paragraph_format.left_indent = (
-                    element.paragraph_format.left_indent
-                )
-                new_paragraph.paragraph_format.right_indent = (
-                    element.paragraph_format.right_indent
-                )
-                new_paragraph.paragraph_format.space_before = (
-                    element.paragraph_format.space_before
-                )
-                new_paragraph.paragraph_format.space_after = (
-                    element.paragraph_format.space_after
-                )
-                new_paragraph.paragraph_format.line_spacing = (
-                    element.paragraph_format.line_spacing
-                )
+    def __copy_file(self, source_file, dest_file):
+        try:
+            shutil.copyfile(source_file, dest_file)
+        except Exception as e:
+            print(f"文件复制失败: {e}")
 
-            if hasattr(element, "runs"):
-                for run in element.runs:
-                    new_run = new_paragraph.add_run(run.text)
-                    new_run.bold = run.bold
-                    new_run.italic = run.italic
-                    new_run.underline = run.underline
-                    if run.font.name:
-                        new_run.font.name = run.font.name
-                    if run.font.size:
-                        new_run.font.size = run.font.size
-                    if run.font.color.rgb:
-                        new_run.font.color.rgb = run.font.color.rgb
-                    new_run.font.highlight_color = run.font.highlight_color
+    def __translate_paragraph(self, index, paragraph):
+        print(f"############# 正在处理第 {index} 个段落 #############")
+        if paragraph.text.strip():
+            translated_text = self.__translate_text(paragraph.text)
+            print(f"原文: {paragraph.text}")
+            print(f"译文: {translated_text}")
+            paragraph.text = paragraph.text.replace(paragraph.text.strip(), translated_text)
 
-        elif element.tag.endswith("tbl"):
-            if hasattr(element, "runs"):
-                new_table = new_doc.add_table(rows=0, cols=0)
-                for row in element.rows:
-                    new_row = new_table.add_row()
-                    for cell_index, cell in enumerate(row.cells):
-                        new_cell = new_row.cells[cell_index]
-                        new_cell.text = cell.text
-                        new_cell_paragraph = new_cell.paragraphs[0]
-                        new_cell_paragraph.alignment = cell.paragraphs[0].alignment
-                        new_cell_paragraph.paragraph_format.left_indent = (
-                            cell.paragraphs[0].paragraph_format.left_indent
-                        )
-                        new_cell_paragraph.paragraph_format.right_indent = (
-                            cell.paragraphs[0].paragraph_format.right_indent
-                        )
-                        new_cell_paragraph.paragraph_format.space_before = (
-                            cell.paragraphs[0].paragraph_format.space_before
-                        )
-                        new_cell_paragraph.paragraph_format.space_after = (
-                            cell.paragraphs[0].paragraph_format.space_after
-                        )
-                        new_cell_paragraph.paragraph_format.line_spacing = (
-                            cell.paragraphs[0].paragraph_format.line_spacing
-                        )
+    def __translate_paragraphs(self, paragraphs):
+        # 只处理前30个
+        # paragraphs = paragraphs[:30]
 
-        elif element.tag.endswith("drawing") or element.tag.endswith("pic"):
-            self.__copy_picture(element, new_doc)
+        self._concurrent_process(
+            paragraphs, self.__translate_paragraph, max_workers=self.num_threads
+        )
 
-    def __copy_picture(self, element, new_doc):
-        """
-        复制图片到新文档中
-        """
-        # 找到所有的图片数据
-        rels = element.part.rels
-        for rel in rels:
-            if "image" in rels[rel].target_ref:
-                image_path = rels[rel].target_part.blob
-                with open(image_path, "rb") as img_file:
-                    image_data = img_file.read()
-                temp_img_path = "temp_img.png"
-                with open(temp_img_path, "wb") as temp_img:
-                    temp_img.write(image_data)
-                new_doc.add_picture(temp_img_path)
-                os.remove(temp_img_path)
+    def __translate_tables(self, tables):
+        paragraphs = []
+        for table in tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    paragraphs.extend(cell.paragraphs)
+        self.__translate_paragraphs(paragraphs)
 
-    def __translate_elements(self, elements, result_list, index):
-        """
-        复制文档元素到一个临时文档，并保存到结果列表中
-        """
-        temp_doc = Document()
-        count = 0
-        for element in elements:
-            self.__translate_element(element, temp_doc)
-            print(f"线程 {index} 处理完第 {index + count} 个段落")
-            count += 1
-        result_list[index] = temp_doc
+    def __translate_headers(self, headers):
+        paragraphs = []
+        for header in headers:
+            paragraphs.extend(header.paragraphs)
+            for table in header.tables:
+                for row in table.rows:
+                    for cell in row.cells:
+                        paragraphs.extend(cell.paragraphs)
+        self.__translate_paragraphs(paragraphs)
+
+    def __translate_footers(self, footers):
+        paragraphs = []
+        for footer in footers:
+            paragraphs.extend(footer.paragraphs)
+            for table in footer.tables:
+                for row in table.rows:
+                    for cell in row.cells:
+                        paragraphs.extend(cell.paragraphs)
+        self.__translate_paragraphs(paragraphs)
+
+    def _concurrent_process(self, items, func, max_workers=16):
+        if len(items) <= 50000:
+            for index, item in enumerate(items):
+                func(index, item)
+        else:
+            # 使用 ThreadPoolExecutor 创建一个包含 max_workers 个线程的线程池
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                # 使用 submit 方法将 items 列表中的每个项目提交给线程池处理
+                futures = [
+                    executor.submit(func, index, item)
+                    for index, item in enumerate(items)
+                ]
+
+                # 等待所有任务完成
+                wait(futures)
 
     def translate(self, file):
+        start = time.time()
+
         # 复制文件
+        translated_file = file.replace(".docx", "-translated.docx")
+        self.__copy_file(file, translated_file)
 
-        # 打开原始文档
-        source_doc = Document(file)
+        doc = Document(translated_file)
 
-        # 获取原文档中的所有元素
-        elements = list(source_doc.element.body.iterchildren())
+        # 翻译页眉
+        headers = [section.header for section in doc.sections]
+        self.__translate_headers(headers)
 
-        # 分成num_threads个部分
-        chunk_size = len(elements) // self.num_threads
-        print(f"共有{len(elements)}个段落，每个线程处理{chunk_size}个段落")
-        chunks = []
-        for i in range(self.num_threads):
-            start = i * chunk_size
-            if i == self.num_threads - 1:  # 最后一个线程处理剩余的所有元素
-                end = len(elements)
-            else:
-                end = (i + 1) * chunk_size
-            chunks.append(list(elements)[start:end])
+        # 翻译页脚
+        footers = [section.footer for section in doc.sections]
+        self.__translate_footers(footers)
 
-        # 创建一个结果列表来保存中间文档
-        results = [None] * self.num_threads
+        # 翻译shapes
+        # self.__translate_paragraphs(doc.inline_shapes)
+        for shape in doc.inline_shapes:
+            print(shape)
 
-        # 创建线程池
-        with ThreadPoolExecutor(max_workers=self.num_threads) as executor:
-            futures = [
-                executor.submit(self.__translate_elements, chunk, results, i)
-                for i, chunk in enumerate(chunks)
-            ]
-            for future in futures:
-                future.result()  # 等待所有线程完成
+        # 翻译段落
+        self.__translate_paragraphs(doc.paragraphs)
 
-        # 创建最终文档并合并所有中间文档
-        new_doc = Document()
-        for temp_doc in results:
-            for element in temp_doc.element.body.iterchildren():
-                self.__translate_element(element, new_doc)
+        # 翻译表格
+        self.__translate_tables(doc.tables)
 
-        # 保存新的文档
-        dest_file = file.replace(".docx", "-translated.docx")
-        new_doc.save(dest_file)
+        # 保存文件
+        doc.save(translated_file)
+
+        end = time.time()
+        print(f"翻译完成，用时: {end - start:.2f} 秒")
 
 
 if __name__ == "__main__":
--- gpt_translator.py
+++ gpt_translator.py
@@ -10,15 +10,15 @@ class GPTTranslator:
         self.client = OpenAI(
             api_key=os.environ.get("OPENAI_API_KEY"),
         )
-        self.system_prompt = "You are a professional translator.The translation should be professional, complete, and must not omit any words. When I ask you for a translation, please reply with the translated text only, without unnecessary descriptions."
+        self.system_prompt = "You are a professional translator.The translation should be professional, complete, and must not omit any words. Do not translate numbers, units of measure, e.g. length, volume, capacity, density, etc. When I ask you for a translation, please reply with the translated text only. If you find there is nothing to translate, just reply with the original text."
 
     def __get_user_prompt(self, text):
-        user_prompt_CN = "Translate the following English text to Chinese:\n\n"
-        user_prompt_EN = "Translate the following Chinese text to English:\n\n"
+        user_prompt_CN = "Translate the following English text to Chinese:"
+        user_prompt_EN = "Translate the following Chinese text to English:"
         return (
-            user_prompt_CN + text + "\n\n"
+            user_prompt_CN + text
             if self.to_CN
-            else user_prompt_EN + text + "\n\n"
+            else user_prompt_EN + text
         )
 
     def __create_system_instruction(self, terminology):
@@ -26,6 +26,9 @@ class GPTTranslator:
         return f"Please use the following translations for specific terms: {terms}."
 
     def translate(self, text):
+        if isinstance(text, list) and len(text) == 0 or isinstance(text, str) and text.strip() == "":
+            return text
+
         system_message = (
             self.system_prompt + self.__create_system_instruction(self.terminology)
             if self.terminology and len(self.terminology) > 0
@@ -39,7 +42,7 @@ class GPTTranslator:
             {
                 "role": "user",
                 "content": self.__get_user_prompt(
-                    repr(text) if type(text) == list else text
+                    repr(text) if isinstance(text, list) else text
                 ),
             },
         ]
@@ -52,15 +55,17 @@ class GPTTranslator:
         )
 
         msg = response.choices[0].message.content.strip()
-        return ast.literal_eval(msg) if type(text) == list else msg
+        if ' -> ' in msg:
+            msg = msg.split(' -> ')[1].strip()
+        return ast.literal_eval(msg) if isinstance(text, list) else msg
 
 
 if __name__ == "__main__":
     translator = GPTTranslator({"understood": "知晓"}, True)
-    text = "I have understood the relevant requirements of GLP regulations with which this study complies and have evaluated the test facility to ensure that the features of the test article and/or reference standard provided are true and accurate. I have also informed the test facility of the safety of the test article and/or reference standard under this study."
+    text = "Weigh 6 g of propylene glycol into a calibrated 100 mL container. After adding 3 g of Kolliphor® HS 15 and stirring until well mixed, add 51.8 mg of the test article. "
     print(translator.translate(text))
-    translator.to_CN = False
-    text = "一只白色的狐狸在森林里跑步."
-    resp = translator.translate(["皮球", "飞机", "彩色蝴蝶在花丛里飞"])
-    for r in resp:
-        print(r)
+    # translator.to_CN = False
+    # text = "一只白色的狐狸在森林里跑步."
+    # resp = translator.translate(["皮球", "飞机", "彩色蝴蝶在花丛里飞"])
+    # for r in resp:
+    #     print(r)
